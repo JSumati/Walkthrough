2aa409e3be12641bbcf5d497fe65e1168007eb00
Sumati Jha <sjha@himalayantechies.com>
2018-03-05 18:04:56 +0000

    Walkthrough-sapUi5

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -6,20 +6,6 @@ sap.ui.define([
 	], function(Controller, MessageToast){
 		"use strict";
 		return Controller.extend("Walkthrough.controller.View1", {
-			// onInit: function(){
-			// 	var oData = {
-			// 		recipient:{
-			// 			name:"World",
-			// 			personality:"Good"
-			// 		}
-			// 	};
-			// 	var oModel = new JSONModel(oData);
-			// 	this.getView().setModel(oModel);
-			// 	var i18nModel = new ResourceModel({
-			// 		bundleName:"Walkthrough.i18n.i18n"
-			// 	});
-			// 	this.getView().setModel(i18nModel, "i18n");
-			// },
 			onShowHello: function(){
 				var oBundle = this.getView().getModel("i18n").getResourceBundle();
 				var sRecipient = this.getView().getModel().getProperty("/recipient/name");
