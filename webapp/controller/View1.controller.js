5e8c47838a027077460cd851d5d03836db39e3b0
Sumati Jha <sjha@himalayantechies.com>
2018-03-13 10:04:14 +0000

    Walkthrough-sapUi5 -- Login Validation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,18 +1,82 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-	], function(Controller){
+	"jquery.sap.global",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/SimpleType",
+	"sap/ui/model/ValidateException",
+	"sap/ui/model/json/JSONModel"
+	], function(jQuery, MessageBox, MessageToast, Controller, SimpleType, ValidateException, JSONModel){
 		"use strict";
 		return Controller.extend("Walkthrough.controller.View1", {
 			onInit: function() {
+					
+				//For the Content Density
 				this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 				
+				var oView = this.getView();
+				oView.setModel(new JSONModel({
+					email: "",
+					password:""
+				}));
+				
+			
 			},
+			preventBack: function(){
+				window.history.forward();
+				window.onunload(null);
+				
+			},
+			
+				
+						
+			
+			//For the Header Open Dialog Button
 			onOpenDialog : function(){
 				this.getOwnerComponent().openHelloDialog();
 			},
+			//For Login Click Button
 			onShowHello: function(oEvent){
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("overview");
-			}
+				//collect input controls
+				var oView = this.getView();
+				var aInputs = [
+					oView.byId("emailInput"),
+					oView.byId("passwordInput")
+					];
+					var bValidationError = false;
+					jQuery.each(aInputs, function(i, oInput){
+					var oBinding = oInput.getBinding("value");
+					try{
+						oBinding.getType().validateValue(oInput.getValue());
+					} catch(oException){
+						oInput.setValueState("Error");
+						bValidationError=true;
+					}
+				});
+				if(!bValidationError){
+						var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+						oRouter.navTo("overview"); 
+						setTimeout("preventBack", 0);
+					
+				} else {
+					MessageBox.alert("A validation error has occured. Complete your Input First");
+				}
+			},
+			customEmailType: SimpleType.extend("email", {
+				formatValue: function(oValue){
+					return oValue;
+				},
+				parseValue: function(oValue){
+					return oValue;
+				},
+				validateValue: function(oValue){
+					var rexMail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+					if(!oValue.match(rexMail)){
+						throw new ValidateException("'" + oValue + "' is not a valid email address");
+					}
+				}
+			
+			})
+					
 		});
 	});
